83ba4cf52e70911d12feadd678f2ae7ac3934d1e
mingmingeeee <45472076+mingmingeeee@users.noreply.github.com>
2023-07-18 22:56:45 +0900

    [level 3] Title: 여행경로, Time: 10.61 ms, Memory: 87 MB -BaekjoonHub

--- 프로그래머스/lv3/43164. 여행경로/여행경로.java
+++ 프로그래머스/lv3/43164. 여행경로/여행경로.java
@@ -1,70 +1,38 @@
 import java.util.*;
 
 class Solution {
-    private static String[] answer;
-    private static Map<String, boolean[]> visited = new HashMap<>();
-    private static int n;
-    private static Map<String, List<String>> map = new HashMap<>();
+    boolean[] visited;
+    ArrayList<String> path;
     public String[] solution(String[][] tickets) {
         
-        n = tickets.length;
-        for(int i = 0; i < tickets.length; i++) {
-            if(map.get(tickets[i][0]) == null) {
-                map.put(tickets[i][0], new ArrayList<>());
-            }
-            map.get(tickets[i][0]).add(tickets[i][1]);
-       
-            
-            Collections.sort(map.get(tickets[i][0]));
-        }
+        String[] answer = {};
+        visited = new boolean[tickets.length];
+        path = new ArrayList<>();
         
-        for(int i = 0; i < tickets.length; i++) {
-            int size = map.get(tickets[i][0]).size();
-     
-            if(visited.get(tickets[i][0]) == null)  
-                visited.put(tickets[i][0], new boolean[size]);
-        }
+        dfs("ICN", "ICN", tickets, 0);
         
-        String sum = "ICN";
-        for(int i = 0; i < map.get("ICN").size(); i++) {
-            String airport = map.get("ICN").get(i);
-            boolean[] v = visited.get("ICN");
-            v[i] = true;
-            dfs(airport, sum + " " + airport, 0);
-            v[i] = false;
-        }
-        
+        Collections.sort(path);
+        answer = path.get(0).split(" ");
     
      
         return answer;
     }
     
-    private static boolean isChecked = false;
-    private static void dfs(String airport, String sum, int cnt) {
+    
+    private void dfs(String start, String route, String[][] tickets, int cnt) {
 
-        if(isChecked)
-            return;
-        if(cnt == n - 1) {
-            answer = sum.split(" ");
-            isChecked = true;
+        if(cnt == tickets.length) {
+            path.add(route);
             return;
         }
-    
-        if(map.get(airport) == null)
-            return;
         
-        for(int i = 0; i < map.get(airport).size(); i++) {
-            
-            String port = map.get(airport).get(i);
-            boolean[] v = visited.get(airport);
-        
-            if(v[i])
-                continue;
-            
-         
-            v[i] = true;
-            dfs(port, sum + " " + port, cnt + 1);
-            v[i] = false;
+        for(int i = 0; i < tickets.length; i++) {
+            if(visited[i]) continue;
+            if(start.equals(tickets[i][0])) {
+                visited[i] = true;
+                dfs(tickets[i][1], route+" "+tickets[i][1], tickets, cnt + 1);
+                visited[i] = false;
+            }
         }
         
         
