e2f6d60b3163cea58f7a7a4e579100adeea95e62
mingmingeeee <45472076+mingmingeeee@users.noreply.github.com>
2023-08-18 04:12:15 +0900

    [Gold III] Title: 사다리 조작, Time: 324 ms, Memory: 16732 KB -BaekjoonHub

--- 백준/Gold/15684. 사다리 조작/사다리 조작.java
+++ 백준/Gold/15684. 사다리 조작/사다리 조작.java
@@ -1,69 +1,87 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.sql.Array;
+import java.util.*;
 
 public class Main {
-    private static int n, m, h, answer;
+
+    private static int N, M, H, ans;
+    private static boolean isFinished = false;
     private static int[][] map;
-    private static boolean finish = false;
 
     public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
-        h = Integer.parseInt(st.nextToken());
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(in.readLine());
 
-        map = new int[h + 1][n + 1];
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        H = Integer.parseInt(st.nextToken());
 
-        int x, y;
-        for (int i = 0; i < m; i++) {
-            st = new StringTokenizer(br.readLine());
-            x = Integer.parseInt(st.nextToken());
-            y = Integer.parseInt(st.nextToken());
-            map[x][y] = 1;
-            map[x][y + 1] = 2;
+        map = new int[H + 1][N + 1];
+
+        for(int i = 0; i < M; i++) {
+            st = new StringTokenizer(in.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            map[a][b] = 1; // 해당칸 기준 오른쪽
+            map[a][b + 1] = 2; // 해당칸 기준 왼쪽
         }
 
-        for (int i = 0; i <= 3; i++) {
-            answer = i;
-            dfs(1, 0);
-            if (finish) break;
+        for(int i = 0; i <= 3; i++) {
+            ans = i;
+            makeLines(1, 0);
+            if(isFinished) break;
         }
 
-        System.out.println((finish) ? answer : -1);
+
+        if(!isFinished) ans = -1;
+        System.out.println(ans);
+
     }
 
-    private static void dfs(int x, int count) {
-        if (finish) return;
-        if (answer == count) {
-            if (check()) finish = true;
+    private static void makeLines(int start, int cnt) {
+
+        if(isFinished) return;
+        if(cnt == ans) {
+            if(check()) isFinished = true;
             return;
         }
 
-        for (int i = x; i < h + 1; i++) {
-            for (int j = 1; j < n; j++) {
-                if (map[i][j] == 0 && map[i][j + 1] == 0) {
+
+        // 가로선 만들기
+        for(int i = start; i <= H; i++) {
+            for(int j = 1; j < N; j++) {
+                if(map[i][j] == 0 && map[i][j + 1] == 0) {
                     map[i][j] = 1;
                     map[i][j + 1] = 2;
-                    dfs(i, count + 1);
-                    map[i][j] = map[i][j + 1] = 0;
+                    makeLines(i, cnt + 1);
+                    map[i][j] = 0;
+                    map[i][j + 1] = 0;
                 }
             }
         }
+
     }
 
     private static boolean check() {
-        for (int i = 1; i <= n; i++) {
-            int x = 1, y = i;
-            for (int j = 0; j < h; j++) {
-                if (map[x][y] == 1) y++;
-                else if (map[x][y] == 2) y--;
+        for(int i = 1; i <= N; i++) {
+            int x = 1; int y = i;
+            while(x <= H) {
+                if(map[x][y] == 1) y++;
+                else if(map[x][y] == 2) y--;
                 x++;
             }
-            if (y != i) return false;
+
+            if(y != i) return false;
         }
+
         return true;
     }
+
 }
+
+// N개의 세로선, M개의 가로선
+// 인접한 세로선 사이 가로선 놓을 수 있음
+// 각각 세로선마다 가로선 놓을 수 있는 위치: H
+// i번 세로선의 결과가 i번 나와야 함 => 추가해야 하는 가로선 개수의 최솟값 구하기
